ffc986cba9fa521cd89d2c82164ff4e0f620ad83
minhvh <minhvh@soft-flight.work>
2019-09-11 17:24:03 +0700

    add vue view

---
+++ webapp/admin_adm/src/store/mutation_constant.js
@@ -0,0 +1,14 @@
+export const UserMutation = {
+  setAvatar: 'setAvatar',
+  setUserId: 'setUserId',
+  setUserName: 'setUserName',
+  setAccess: 'setAccess',
+  setToken: 'setToken',
+  setHasGetInfo: 'setHasGetInfo',
+  setMessageCount: 'setMessageCount',
+  setMessageUnreadList: 'setMessageUnreadList',
+  setMessageReadedList: 'setMessageReadedList',
+  setMessageTrashList: 'setMessageTrashList',
+  updateMessageContentStore: 'updateMessageContentStore',
+  moveMsg: 'moveMsg',
+};
