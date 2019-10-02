76a2ba77a05c18a39df8b57cd7ecdf644ba47ca1
minhvh <minhvh@soft-flight.work>
2019-10-02 17:31:05 +0700

    init

--- controllers/HomeController.go
+++ controllers/HomeController.go
@@ -3,16 +3,14 @@ package controllers
 import (
 	"adm/model"
 	"adm/pkg/web"
+	"encoding/json"
 )
 
 type HomeController struct{}
 
 func (t *HomeController) Index(v *web.View) {
 	md := model.Account{}
-
-
-	v.Data.Set("users", md.GetAllUser())
-
-
+	users,_ := json.Marshal(md.GetAllUser())
+	v.Data.Set("users", string(users))
 	v.Render("home")
 }
